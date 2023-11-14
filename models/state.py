4549eb28c2730a7697d5bf34cfa910c4ceb824e9
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-14 16:39:34 -0600

    reverting to previous storage methos

--- models/state.py
+++ models/state.py
@@ -13,3 +13,13 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """returns list of City instances upon state_id"""
+            cities_list = []
+            for obj in list(models.storage.all(City).values()):
+                if obj.state_id == self.id:
+                    cities_list.append(obj)
+            return cities_list
