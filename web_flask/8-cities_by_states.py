411c6dd0f74251fefa87b9d3501689730846bbd0
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-14 15:35:01 -0600

    simplified cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,9 +3,9 @@
 This is a Flask web application.
 """
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
@@ -13,14 +13,8 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Display a list of states and cities from the storage """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-
-    for state in sorted_states:
-        if storage.__class__.__name__ == "DBStorage":
-            state.cities = state.cities()
-
-    return render_template('7-cities_by_states.html', states=sorted_states)
+    states = storage.all('State')
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
