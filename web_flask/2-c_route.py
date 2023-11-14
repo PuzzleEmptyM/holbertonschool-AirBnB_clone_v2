36a65db66ac63ee5cfd0b842b8fbae50f8c527f4
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-11-14 12:21:27 -0600

    Now, there's a new route /c/<text> that displays "C " followed by the value of the text variable.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is a simple Flask web application.
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    # Replace underscores with spaces and display "C " followed by the value of the text variable
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
